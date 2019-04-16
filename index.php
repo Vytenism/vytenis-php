5dd516a80df7410a311334126510b2dc060fc62f
vytenism <aparance23@gmail.com>
2019-04-16 17:30:42 +0300

    third commit

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html>
     <head>
         <meta charset="8">
-        <title><?php //Komentarai; ?></title>    
+        <title>Komentarai</title>    
     </head>
     <body>
-        <h1><?php // Dainu tekstai; ?> </h1>
-        <h2><?php // paragrafo daina ?></h2>
-        <p><?php // As toks vienisas jaunas paragrafas... ; ?></p>
-        <p><?php // Mindau svetimo kodo gatves... ?></p>
+        <h1>Dainu tekstai</h1>
+        <h2> paragrafo daina</h2>
+        <p>As toks vienisas jaunas paragrafas...</p>
+        <p><?php // Mindau svetimo kodo gatves... ; ?></p>
     </body>
 </html>
 
