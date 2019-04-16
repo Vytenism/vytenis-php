e9913ec22d8dda9faac108072f97d6e0aa81e5ee
vytenism <aparance23@gmail.com>
2019-04-16 16:25:40 +0300

    third commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <meta charset="8">
-        <title><?php print 'As,' . date("l") . 'ir' . 'PHP'; ?></title>    
+        <title><?php print 'As,' . date("l") . ' ir PHP'; ?></title>    
     </head>
     <body>
         <h1>Vytenis - HTML <?php print 'ir PHP'; ?> asas jau nuo <?php print date("Y") . ' metu'; ?> </h1>
