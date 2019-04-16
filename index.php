589a072cadfdb7a161a6c3eaf9eb27bd5845a5f8
vytenism <aparance23@gmail.com>
2019-04-16 15:00:55 +0300

    second commit

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<?php 
-print 'Veikia man!';
+<<html>
+    <head>
+        <title>As valdau HTML</title>
+    </head>
+    <body>
+        <h1>Vytenis HTML asas!</h1>
+        <p>Moku kurti statini turini.</p>
+    </body>
+</html>
+
