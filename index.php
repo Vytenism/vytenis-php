4cc2799fda840e4a65bdc6fdc6220fb5cca13902
vytenism <aparance23@gmail.com>
2019-04-16 15:07:21 +0300

    third commit

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-<<html>
+<html>
     <head>
         <title>As valdau HTML</title>
+        <meta charset="8">
+        <link rel="stylesheet" type="text/css" href="style.css"
     </head>
     <body>
         <h1>Vytenis HTML asas!</h1>
