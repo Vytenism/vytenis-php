e64c3da753f323e735f34cc4d343b4edf4df3cf6
vytenism <aparance23@gmail.com>
2019-04-16 16:17:54 +0300

    third commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <title><?php print 'As,' . date("l") . 'ir' . 'PHP'; ?></title>    
     </head>
     <body>
-        <h1>Vytenis - HTML <?php print 'ir PHP'; ?> asas jau nuo<?php print date("Y") . 'metu'; ?> </h1>
-        <p>Viskas prasidejo <?php print date("m") . 'menesio,' . date("d") . 'diena'; ?></p>
+        <h1>Vytenis - HTML <?php print 'ir PHP'; ?> asas jau nuo <?php print date("Y") . ' metu'; ?> </h1>
+        <p>Viskas prasidejo <?php print date("m") . ' menesio, ' . date("d") . ' diena'; ?></p>
     </body>
 </html>
