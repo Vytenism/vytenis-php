2d16c1c513edd7b869bf3ea56363c4b04d5989a8
vytenism <aparance23@gmail.com>
2019-04-17 16:41:13 +0300

    one

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
-<?php
-$img = 'monkey.jpg';
-$img2 = 'monkey2.jpg';
-$img3 = 'monkey3.jpg';
-$img4 = 'monkey4.jpg';
+<?php 
+
+    $sudas1 = (rand(1,100));
+    $sudas2 = (rand(101,200));
+    $sudas3 = (rand(201,300));
+    $sudas4 = (rand(301,400));
+    
 ?>
-<html>
-    <head>
-        <title>title</title>
-    </head>
-    <body>
-        <img src="<?php print $img; ?>">
-        <img src="<?php print $img2; ?>">
-        <img src="<?php print $img3; ?>">
-        <img src="<?php print $img4; ?>">
-    </body>
-</html>
+<div class="body1">
+    <h1>Skolos skaicuokle</h1>
+    <h3>Jei paemei <?php print $sudas1;?> jievru</h3>
+    <h3>Su dviem kabanciais grazinsi <?php print $sudas3?></h3>
+    <h3>Su vienu kabanciu grazinsi <?php print $sudas2;?></h3>
+    </div>
