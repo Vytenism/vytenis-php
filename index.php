2511fe9318d1902138e185d2c598d9bdc170cc8b
vytenism <aparance23@gmail.com>
2019-04-17 11:38:20 +0300

    five commit

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
-
+<?php
+date_default_timezone_set('europe/vilnius')
+?>
 <html>
     <head>
         <meta charset="8">
-        <title>Komentarai</title>    
+        <title>PHP lydes ir </title>    
     </head>
-    <body>
-        <h1><?php //Dainu tekstai; ?></h1>
-        <h2> paragrafo daina</h2>
-        <p>As toks vienisas jaunas paragrafas...</p>
-        <p> Mindau svetimo kodo gatves... </p>
+    <body style='background-color: rgb(<?php print rand(000, 255) . ',' . rand(000, 255) . ',' . rand(000, 255); ?>)'>
+        <h1 style="font-size: <?php print rand(0, 300); ?>%">
+            As keiciu savo dydi!
+        </h1>
+        <p style="color: rgb(<?php print rand(000, 255) . ',' . rand(000, 255) . ',' . rand(000, 255); ?>); font-size: <?php print rand(0, 300); ?>%">
+            As keiciu savo spalva!
+        </p>
+        <img src="dice/<?php print rand(1, 6); ?>.png">
     </body>
 </html>
 
