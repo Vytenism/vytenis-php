7181bf6adc8a96cce54e07ab77794d7241cf9b39
vytenism <aparance23@gmail.com>
2019-04-17 16:49:11 +0300

    one

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
 
-    $integer1 = rand(1,100);
-    $integer2 = rand(101,200);
-    $integer3 = rand(201,300);
-    $integer4 = rand(301,400);
+    $eur1 = rand(1,100);
+    $eur2 = rand(101,200);
+    $eur3 = rand(201,300);
+    $eur4 = rand(301,400);
     
 ?>
 <html>
@@ -13,9 +13,9 @@
     <body>
     <div>
         <h1>Skolos skaicuokle</h1>
-        <h3>Jei paemei <?php print $integer1;?> jievru</h3>
-        <h3>Su dviem kabanciais grazinsi <?php print $integer3;?></h3>
-        <h3>Su vienu kabanciu grazinsi <?php print $integer2;?></h3>
+        <h3>Jei paemei <?php print $eur1;?> jievru</h3>
+        <h3>Su dviem kabanciais grazinsi <?php print $eur3;?></h3>
+        <h3>Su vienu kabanciu grazinsi <?php print $eur2;?></h3>
     </div>
     </body>
 </html>
