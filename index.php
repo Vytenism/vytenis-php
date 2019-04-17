b1da9ea9f74187c942b6821b7f06c7ac18382e1d
vytenism <aparance23@gmail.com>
2019-04-17 16:15:15 +0300

    one

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 $img = 'monkey.jpg';
+$img2 = 'monkey2.jpg';
+$img3 = 'monkey3.jpg';
+$img4 = 'monkey4.jpg';
 ?>
 <html>
     <head>
@@ -7,8 +10,8 @@ $img = 'monkey.jpg';
     </head>
     <body>
         <img src="<?php print $img; ?>">
-        <img src="<?php print $img; ?>">
-        <img src="<?php print $img; ?>">
-        <img src="<?php print $img; ?>">
+        <img src="<?php print $img2; ?>">
+        <img src="<?php print $img3; ?>">
+        <img src="<?php print $img4; ?>">
     </body>
 </html>
