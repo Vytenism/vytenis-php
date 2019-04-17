44f3146a1a2d3d16d8e6395e41cb0a23f0757848
vytenism <aparance23@gmail.com>
2019-04-17 14:41:36 +0300

    one

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
 <?php
-date_default_timezone_set('europe/vilnius')
-?>
-<html>
-    <head>
-        <meta charset="8">
-        <title>PHP lydes ir </title>    
-    </head>
-    <body style='background-color: rgb(<?php print rand(000, 255) . ',' . rand(000, 255) . ',' . rand(000, 255); ?>)'>
-        <h1 style="font-size: <?php print rand(0, 300); ?>%">
-            As keiciu savo dydi!
-        </h1>
-        <p style="color: rgb(<?php print rand(000, 255) . ',' . rand(000, 255) . ',' . rand(000, 255); ?>); font-size: <?php print rand(0, 300); ?>%">
-            As keiciu savo spalva!
-        </p>
-        <img src="dice/<?php print rand(1, 6); ?>.png">
-    </body>
-</html>
+$tu = 123;
+$as = 'masina';
+$jis = 3.11;
+$mes = false;
+$jie = NULL;
 
+print $tu;
+print $as;
+print $jis;
+print $mes;
+print $jie;
+?>
