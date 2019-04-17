1ca21fbc4f44bf64f6473cb100157b19dae8c733
vytenism <aparance23@gmail.com>
2019-04-17 17:14:38 +0300

    one

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
-<?php 
+<?php
 
-    $eur1 = rand(1,100);
-    $eur2 = rand(101,200);
-    $eur3 = rand(201,300);
-        
+$eur1 = rand(1, 100);
+$eur2 = rand(101, 200);
+$eur3 = rand(201, 300);
+
 ?>
 <html>
     <head>
-        <meta charset="8">
+        <meta charset="UTF-8">
         <title>title</title>
     </head>
     <body>
-    <div>
         <h1>Skolos skaicuokle</h1>
-        <h3>Jei paemei <?php print $eur1;?> jievru</h3>
-        <h3>Su dviem kabanciais grazinsi <?php print $eur3;?></h3>
-        <h3>Su vienu kabanciu grazinsi <?php print $eur2;?></h3>
-    </div>
+        <h2>Jei paemei <?php print $eur1; ?> jievru</h2>
+        <h3>Su dviem kabanciais grazinsi <?php print $eur3; ?></h3>
+        <h3>Su vienu kabanciu grazinsi <?php print $eur2; ?></h3>
     </body>
 </html>
