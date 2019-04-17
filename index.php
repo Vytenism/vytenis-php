70d4c537acf6dbb12bc5a0a2320fe9f184b984ce
vytenism <aparance23@gmail.com>
2019-04-17 16:44:29 +0300

    one

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php 
 
-    $sudas1 = (rand(1,100));
-    $sudas2 = (rand(101,200));
-    $sudas3 = (rand(201,300));
-    $sudas4 = (rand(301,400));
+    $integer1 = rand(1,100);
+    $integer2 = rand(101,200);
+    $integer3 = rand(201,300);
+    $integer4 = rand(301,400);
     
 ?>
-<div class="body1">
-    <h1>Skolos skaicuokle</h1>
-    <h3>Jei paemei <?php print $sudas1;?> jievru</h3>
-    <h3>Su dviem kabanciais grazinsi <?php print $sudas3?></h3>
-    <h3>Su vienu kabanciu grazinsi <?php print $sudas2;?></h3>
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+    <div class="body1">
+        <h1>Skolos skaicuokle</h1>
+        <h3>Jei paemei <?php print $integer1;?> jievru</h3>
+        <h3>Su dviem kabanciais grazinsi <?php print $integer3;?></h3>
+        <h3>Su vienu kabanciu grazinsi <?php print $integer2;?></h3>
     </div>
+    </body>
+</html>
