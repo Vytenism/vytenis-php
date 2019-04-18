a8bb7395a143d15ddda2b1ccbdc1d97e79f4e32f
vytenism <aparance23@gmail.com>
2019-04-18 17:08:02 +0300

    ifai

--- index.php
+++ index.php
@@ -1,19 +1,69 @@
 <?php
+$sunny = 1;
 
-$eur1 = rand(1, 100);
-$eur2 = rand(101, 200);
-$eur3 = rand(201, 300);
+if ($sunny) {
+    print 'sauleta' . '<br>';
+} else {
+    print 'debesuota' . '<br>';
+}
 
+$x = 1;
+$y = 2;
+
+if ($x + $y === 3)
+    print 'sauleta' . '<br>';
+
+$x = 0;
+
+if ($x == 1) {
+    print 'x yra 1' . '<br>';
+} elseif ($x == 2) {
+    print 'x yra 2' . '<br>';
+} else {
+    print 'nei vienas' . '<br>';
+}
+
+$distance = rand(10, 10000);
+$consumption = 7.5;
+$price_1 = 1.3;
+$money = 100;
+$cons = $consumption * $distance / 100;
+$price = round(1.3 * $cons);
+$rez = '';
+if ($money >= $price) {
+    $rez = 'negaliu';
+} elseif ($money <= $price) {
+    $rez = 'galiu';
+}
+
+$velai = rand(0, 1);
+$girtas = rand(0, 1);
+
+
+if ($velai && $girtas) {
+    $velgir = 'miegosi ant sofos';
+} elseif ($velai && !$girtas) {
+    $velgir = 'negrizai girtas';
+} elseif (!$velai && $girtas) {
+$velgir = 'nemiegosi ant sofos nes negrizai velai';
+} else {
+    $velgir = 'nemiegosi ten';
+}
+
+
+;
 ?>
+
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title>title</title>
+        <title>asa</title>
     </head>
     <body>
-        <h1>Skolos skaicuokle</h1>
-        <h2>Jei paemei <?php print $eur1; ?> jievru</h2>
-        <h3>Su dviem kabanciais grazinsi <?php print $eur3; ?></h3>
-        <h3>Su vienu kabanciu grazinsi <?php print $eur2; ?></h3>
+        <p>
+            <?php print $velgir; ?>
+        </p>
     </body>
 </html>
+
+
+
